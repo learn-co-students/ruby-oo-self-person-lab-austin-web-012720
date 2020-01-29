08be74b2e089791e31af13b96d05bb12c7fc076d
github username <github email address>
2020-01-29 12:26:21 -0600

    First commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
-# your code goes here
+class Person
+    attr_reader :name 
+    attr_accessor :bank_account, :happiness
+    def initialize(name)
+        @name = name 
+        @bank_account = 25
+        @happiness = 8
+    end
+
+   def happiness=(happiness_level)
+        if happiness_level > 10
+            @happiness = 10 
+        elsif happiness_level < 0
+            @happiness = 0 
+        else
+            @happiness = happiness_level
+        end
+
+   end
+
+
+
+end
+
